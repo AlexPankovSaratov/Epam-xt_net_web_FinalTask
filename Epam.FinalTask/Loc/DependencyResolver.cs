51ab6fbf85db1d822ba357927bca95177bd623d3
PankovAlex <pankov92@mail.ru>
2020-02-23 15:15:44 +0400

    Create Final Task

---
+++ Epam.FinalTask/Loc/DependencyResolver.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loc
+{
+    class DependencyResolver
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
